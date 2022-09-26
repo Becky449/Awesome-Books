1ee62d94e7a779f98217048e87e990660e6a4943
Nedramrez <zermarden@gmail.com>
2022-09-26 21:30:56 +0600

    Add local storage code to script.js

--- script.js
+++ script.js
@@ -3,12 +3,47 @@ const form = document.querySelector('form');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const submit = document.getElementById('submit');
+const body = document.querySelector('body');
 
-function newBook(){
+/* function newBook(){
     let book = {};
     book = {
         title: title,
         author: author
     }
     books.push(book);
+} */
+
+const retrieveData = localStorage.getItem('book');
+
+body.onload = () => {
+    if (retrieveData) {
+    const serialize = JSON.parse(retrieveData);
+    title.value = serialize.text;
+    author.value = serialize.text;
 }
+};
+
+document.querySelectorAll('input').forEach((input) => {
+    input.addEventListener('input', (event) => {
+    event.preventDefault();
+
+      // Get input field values
+    const titleData = document.querySelector('#title').value;
+    const authorData = document.querySelector('#author').value
+
+      // Store values in object;
+    const bookData = {
+        title: titleData,
+        author: authorData,
+    };
+
+      // store the object in localStorage
+
+    localStorage.setItem('book', JSON.stringify(bookData));
+    });
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
